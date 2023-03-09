a2bb5bbd1099360c383dbd75b0ba199443b47602
yokoyama <yokoyama@DESKTOP-5P6MFQI.localdomain>
2023-03-09 16:46:23 +0900

    シードデータ作成/ビュー修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,40 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
 User.create!(name: "admin", email: 'admin@gmail.com', password: '111111',
              password_confirmation: '111111', admin: true)
+
+10.times do |n|
+  name = Faker::Name.first_name 
+  email = Faker::Internet.email
+  password = "password"
+  User.create!(name: name,
+                email: email,
+                password: password,
+                )
+end
+
+create_start_day = Date.new(2023, 3, 10)
+create_end_day = Date.new(2023, 3, 31)
+
+
+
+10.times do |n|
+   
+  email = Faker::Internet.email
+  password = "password"
+  Task.create!(task_name: "万葉課題",
+                detail: email,
+                deadline: rand(create_start_day..create_end_day),
+                priority: rand(0..2),
+                status: "b",
+                user_id: users.sample)
+end
+
+
+
+10.times do |n|
+name = Faker::Games::Pokemon.name
+Label.create!(label_name: name)
+end
---
+++ spec/factories/labels.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :label do
+    label_name { "MyString" }
+  end
+end
